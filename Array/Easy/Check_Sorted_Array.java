fa3a0e6b68528cd551d0f01751a5b16f540c9eae
Ankit Kumar <77051500+ankitkumar6207@users.noreply.github.com>
2023-10-28 16:35:36 +0530

    Create Check_Sorted_Array.java

---
+++ Array/Easy/Check_Sorted_Array.java
@@ -0,0 +1,45 @@
+/*
+Check Sorted Array
+input:
+4
+0 0 0 1
+output:
+1
+explanation:
+The given array is sorted in non-decreasing order; hence the answer will be 1. 
+
+
+very easy question so directly optimal solution 
+optimal : TC = O(n)
+1.run a loop from i=1
+2.run a loop and check if the element is greater than or equal to the previous element then it 
+is sorted.
+
+*/
+public class Check_Sorted_Array {
+    public static int isSorted(int n, int []a) {
+        for(int i=1;i<n-1;i++)
+        {
+            if(a[i]>=a[i-1])
+            {
+              
+            }
+            else return 0;
+        }
+        return 1;
+    }
+
+    public static void main(String ar[])
+    {
+        int arr[]={1,2,3,3,5,6,7,8};
+        int n = arr.length;
+        int rs = isSorted(n,arr);
+        if(rs == 1)
+        {
+            System.out.println("sorted");
+        }
+        else{
+            System.out.print("unsorted");
+        }
+    }
+}
