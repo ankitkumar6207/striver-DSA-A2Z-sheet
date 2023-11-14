41a64d7449245658cf45532bcd2360aa888978de
Ankit Kumar <77051500+ankitkumar6207@users.noreply.github.com>
2023-11-14 16:50:19 +0530

    Update Left_Rotate_Array_By_D_Place.java

--- Array/Easy/Left_Rotate_Array_By_D_Place.java
+++ Array/Easy/Left_Rotate_Array_By_D_Place.java
@@ -88,6 +88,67 @@ reverse(a+d,a+n);
 reverse(a,a+n);
 */
 
+// left rotaion by d places:
+// #brute force 
+  class Solution {
+    public void rotate(int[] nums, int k) {
+        int n=nums.length;
+        k%=nums.length;
+        int temp[] = new int[k];
+    
+    // left rotation : 
+        // for(int i=0;i<k;i++)
+        // {
+        //     temp[i] = nums[i];
+        // }
+        // for(int i=k;i<n;i++)
+        // {
+        //     nums[i-k] = nums[i];
+        // }
+
+        // for(int i = n-k ; i < n ; i++)
+        // {
+        //     nums[i] = temp[i- (n - k)];
+        // }
+
+        // right rotation : brute force
+            
+        // for (int i = n - k; i < n; i++) {
+        // temp[i - n + k] = nums[i];
+        // }
+        // for (int i = n - k - 1; i >= 0; i--) {
+        // nums[i + k] = nums[i];
+        // }
+        // for (int i = 0; i < k; i++) {
+        // nums[i] = temp[i];
+        // }
+
+
+        // optimal : right rotation :
+        // public void rotate(int[] nums, int k) {
+        // k %= nums.length;
+        // int n = nums.length;
+        // reverseNum(nums,0,n-1);
+        // reverseNum(nums,0,k-1);
+        // reverseNum(nums,k,n-1);
+
+        // optimal : left rotation 
+        reverseNum(nums,0,k-1);
+        reverseNum(nums,k,n-1);
+        reverseNum(nums,0,n-1);
+    }
+    public void reverseNum(int[] nums, int start, int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    
+    }
+}
+
 
 
 
